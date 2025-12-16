37f81798251040b37f1505c5a6f318c5fd7afb47
DominicDev <s0l0estudi4r@gmail.com>
2025-12-16 12:47:29 +0100

    v0.1.5

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -3,103 +3,120 @@ import { baseUrl } from '@/utils/functions';
 export const arrayCards = [
   {
     title: 'Jump',
-    canvaWeb: 'https://www.canva.com/design/DAG2zN9sKVI/JPGxx_WkE3GmA2zMtJbIGQ/view?utm_content=DAG2zN9sKVI&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h0b98b4c908',
+    canvaWeb:
+      'https://www.canva.com/design/DAG2zN9sKVI/JPGxx_WkE3GmA2zMtJbIGQ/view?utm_content=DAG2zN9sKVI&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h0b98b4c908',
     localImage: '/assets/jump.avif',
     id: '01'
   },
   {
     title: 'Curve Text',
-    canvaWeb: 'https://www.canva.com/design/DAG3ASdM3mw/mlXKEzuUh9zoCXH0cGR6bQ/view?utm_content=DAG3ASdM3mw&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hcd5298899d',
+    canvaWeb:
+      'https://www.canva.com/design/DAG3ASdM3mw/mlXKEzuUh9zoCXH0cGR6bQ/view?utm_content=DAG3ASdM3mw&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hcd5298899d',
     localImage: '/assets/CurveText.avif',
     id: '02'
   },
   {
     title: 'TypeCurve Plugin',
-    canvaWeb: 'https://www.canva.com/design/DAG3Adt0Yek/pvB4MuFFbWzc3HqySbp_Iw/view?utm_content=DAG3Adt0Yek&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h000a542345',
+    canvaWeb:
+      'https://www.canva.com/design/DAG3Adt0Yek/pvB4MuFFbWzc3HqySbp_Iw/view?utm_content=DAG3Adt0Yek&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h000a542345',
     localImage: '/assets/TypeCurvePlugin.avif',
     id: '03'
   },
   {
     title: 'Code QR with Canvas',
-    canvaWeb: 'https://www.canva.com/design/DAG3qfvzJrI/-wqnBHUZirWFxVgq7oi3tA/view?utm_content=DAG3qfvzJrI&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h846f9f08a7',
+    canvaWeb:
+      'https://www.canva.com/design/DAG3qfvzJrI/-wqnBHUZirWFxVgq7oi3tA/view?utm_content=DAG3qfvzJrI&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h846f9f08a7',
     localImage: '/assets/code-qr-with-canvas.avif',
     id: '04'
   },
   {
     title: 'Warp Bush App Practice',
-    canvaWeb: 'https://www.canva.com/design/DAG3u422fLw/jqmEHtGoI-sKzFRhzmTF9A/view?utm_content=DAG3u422fLw&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hd7a71d6d34',
+    canvaWeb:
+      'https://www.canva.com/design/DAG3u422fLw/jqmEHtGoI-sKzFRhzmTF9A/view?utm_content=DAG3u422fLw&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hd7a71d6d34',
     localImage: '/assets/warp-bush.avif',
     id: '05'
   },
   {
     title: 'IceCream Poster',
-    canvaWeb: 'https://www.canva.com/design/DAG36tir4gE/hfCX0ZALjKjSYRZSBbo1og/view?utm_content=DAG36tir4gE&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h44a7795cee',
+    canvaWeb:
+      'https://www.canva.com/design/DAG36tir4gE/hfCX0ZALjKjSYRZSBbo1og/view?utm_content=DAG36tir4gE&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h44a7795cee',
     localImage: '/assets/icecream-poster.avif',
     id: '06'
   },
   {
     title: 'Using Image Blender Plugin',
-    canvaWeb: 'https://www.canva.com/design/DAG4H2pjH9c/ApVc3oOJER_tsaUWVmmeaw/view?utm_content=DAG4H2pjH9c&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h6f37b6aec2',
+    canvaWeb:
+      'https://www.canva.com/design/DAG4H2pjH9c/ApVc3oOJER_tsaUWVmmeaw/view?utm_content=DAG4H2pjH9c&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h6f37b6aec2',
     localImage: '/assets/image-blender-plugin.avif',
     id: '07'
   },
   {
     title: 'Easy Reflection Plugin',
-    canvaWeb: 'https://www.canva.com/design/DAG4SjVJ55s/j0aI1P3lDDvqYXhumIHmRQ/view?utm_content=DAG4SjVJ55s&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h39d2d18fba',
+    canvaWeb:
+      'https://www.canva.com/design/DAG4SjVJ55s/j0aI1P3lDDvqYXhumIHmRQ/view?utm_content=DAG4SjVJ55s&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h39d2d18fba',
     localImage: '/assets/easy-reflection-plugin.avif',
     id: '08'
   },
   {
     title: 'Proving Vectorizer App',
-    canvaWeb: 'https://www.canva.com/design/DAG4gA4_xAM/j4cKJi3ol7S2NF5oMUDIBw/view?utm_content=DAG4gA4_xAM&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h7e5606651b',
+    canvaWeb:
+      'https://www.canva.com/design/DAG4gA4_xAM/j4cKJi3ol7S2NF5oMUDIBw/view?utm_content=DAG4gA4_xAM&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h7e5606651b',
     localImage: '/assets/proving-vectorizer-app.avif',
     id: '09'
   },
   {
     title: 'Clipping Mask App',
-    canvaWeb: 'https://www.canva.com/design/DAG4r2uqbA8/mnuHMLrkqDigSDvl5m7mwg/view?utm_content=DAG4r2uqbA8&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h82085eb5bc',
+    canvaWeb:
+      'https://www.canva.com/design/DAG4r2uqbA8/mnuHMLrkqDigSDvl5m7mwg/view?utm_content=DAG4r2uqbA8&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h82085eb5bc',
     localImage: '/assets/clipping-mask-app.avif',
     id: '10'
   },
   {
     title: 'Sarcoma e Mioma',
-    canvaWeb: 'https://www.canva.com/design/DAG4whX5zYQ/yD238C-sSEz9b8IJU8vmYQ/view?utm_content=DAG4whX5zYQ&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hc706d3d4ec',
+    canvaWeb:
+      'https://www.canva.com/design/DAG4whX5zYQ/yD238C-sSEz9b8IJU8vmYQ/view?utm_content=DAG4whX5zYQ&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hc706d3d4ec',
     localImage: '/assets/sarcoma-e-mioma-ppt.avif',
     id: '11'
   },
   {
     title: 'Image Divider App',
-    canvaWeb: 'https://www.canva.com/design/DAG43SzY_3Y/hjOBPzyWS4qs0H9VOCLZbg/view?utm_content=DAG43SzY_3Y&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h74b75717ef',
+    canvaWeb:
+      'https://www.canva.com/design/DAG43SzY_3Y/hjOBPzyWS4qs0H9VOCLZbg/view?utm_content=DAG43SzY_3Y&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h74b75717ef',
     localImage: '/assets/image-divider-app.avif',
     id: '12'
   },
   {
     title: 'Image Cutout App',
-    canvaWeb: 'https://www.canva.com/design/DAG5CoN1nek/iePo_wGgFO8ztc80lwShnQ/view?utm_content=DAG5CoN1nek&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h72602e0004#1',
+    canvaWeb:
+      'https://www.canva.com/design/DAG5CoN1nek/iePo_wGgFO8ztc80lwShnQ/view?utm_content=DAG5CoN1nek&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h72602e0004#1',
     localImage: '/assets/image-cutout-app.avif',
     id: '13'
   },
   {
     title: 'App coloring book',
-    canvaWeb: 'https://www.canva.com/design/DAG5Pj5W__4/QCfTiGkCmfxHqfRjqx3Czw/view?utm_content=DAG5Pj5W__4&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h37569fa134',
+    canvaWeb:
+      'https://www.canva.com/design/DAG5Pj5W__4/QCfTiGkCmfxHqfRjqx3Czw/view?utm_content=DAG5Pj5W__4&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h37569fa134',
     localImage: '/assets/app-coloring-book.avif',
     id: '14'
   },
   {
     title: 'Over Design',
-    canvaWeb: 'https://www.canva.com/design/DAG5awkB7Mk/M3Ta7NExJMmxx024fI2TUQ/watch?utm_content=DAG5awkB7Mk&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h9511a998d0',
+    canvaWeb:
+      'https://www.canva.com/design/DAG5awkB7Mk/M3Ta7NExJMmxx024fI2TUQ/watch?utm_content=DAG5awkB7Mk&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h9511a998d0',
     localImage: '/assets/over-design.avif',
     id: '15'
   },
   {
     title: 'Shopify Story Ig Practice',
-    canvaWeb: 'https://www.canva.com/design/DAG5awNsc7k/v5nVOg1B78b8wwmVzHiKSA/watch?utm_content=DAG5awNsc7k&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h41c187852b',
+    canvaWeb:
+      'https://www.canva.com/design/DAG5awNsc7k/v5nVOg1B78b8wwmVzHiKSA/watch?utm_content=DAG5awNsc7k&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h41c187852b',
     localImage: '/assets/shopify-ig-story-practice.avif',
     id: '16'
   },
   {
     title: 'Grid',
-    canvaWeb: 'https://www.canva.com/design/DAG5awHzOJo/NyplPzFxRjFZ5FVjZf_aJg/watch?utm_content=DAG5awHzOJo&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h6fd08b894d',
+    canvaWeb:
+      'https://www.canva.com/design/DAG5awHzOJo/NyplPzFxRjFZ5FVjZf_aJg/watch?utm_content=DAG5awHzOJo&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h6fd08b894d',
     localImage: '/assets/grid.avif',
     id: '17'
   },
@@ -111,94 +128,109 @@ export const arrayCards = [
   },
   {
     title: 'Blend Image Screen',
-    canvaWeb: 'https://www.canva.com/design/DAG5mjy-9Xg/oFydVIdjIBY7hD4ZMMx4nA/view?utm_content=DAG5mjy-9Xg&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h2038a58195',
+    canvaWeb:
+      'https://www.canva.com/design/DAG5mjy-9Xg/oFydVIdjIBY7hD4ZMMx4nA/view?utm_content=DAG5mjy-9Xg&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h2038a58195',
     localImage: '/assets/blend-image-screen.avif',
     id: '19'
   },
   {
     title: 'Flups Slider',
-    canvaWeb: 'https://www.canva.com/design/DAG5m8eHb50/dXv2OuRm5immgazpiKZVFQ/view?utm_content=DAG5m8eHb50&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hdd79a20664',
+    canvaWeb:
+      'https://www.canva.com/design/DAG5m8eHb50/dXv2OuRm5immgazpiKZVFQ/view?utm_content=DAG5m8eHb50&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hdd79a20664',
     localImage: '/assets/flups-slider.avif',
     id: '20'
   },
   {
     title: 'Paris Post',
-    canvaWeb: 'https://www.canva.com/design/DAG5yimCGvI/bcgh53q3kqynL1manaG7lw/view?utm_content=DAG5yimCGvI&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hd72e0d2be3',
+    canvaWeb:
+      'https://www.canva.com/design/DAG5yimCGvI/bcgh53q3kqynL1manaG7lw/view?utm_content=DAG5yimCGvI&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hd72e0d2be3',
     localImage: '/assets/paris-post.avif',
     id: '21'
   },
   {
     title: 'App Foto FoldX',
-    canvaWeb: 'https://www.canva.com/design/DAG5_FRj1-Q/APou6G0nmLvrTyx7E7ARNg/view?utm_content=DAG5_FRj1-Q&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hb58a8f0bf5',
+    canvaWeb:
+      'https://www.canva.com/design/DAG5_FRj1-Q/APou6G0nmLvrTyx7E7ARNg/view?utm_content=DAG5_FRj1-Q&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hb58a8f0bf5',
     localImage: '/assets/app-foto-foldx.avif',
     id: '22'
   },
   {
     title: 'App Device Mockup',
-    canvaWeb: 'https://www.canva.com/design/DAG5_C_GN5o/yDtEtn0zlrvSpsIR4MNAcQ/view?utm_content=DAG5_C_GN5o&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hc018bae20b',
+    canvaWeb:
+      'https://www.canva.com/design/DAG5_C_GN5o/yDtEtn0zlrvSpsIR4MNAcQ/view?utm_content=DAG5_C_GN5o&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hc018bae20b',
     localImage: '/assets/app-device-mockup.avif',
     id: '23'
   },
   {
     title: 'Cartoonify App',
-    canvaWeb: 'https://www.canva.com/design/DAG6LdZ1yF0/gk1sGP3CixsWWbaYPO-7WA/view?utm_content=DAG6LdZ1yF0&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h77b333fb75',
+    canvaWeb:
+      'https://www.canva.com/design/DAG6LdZ1yF0/gk1sGP3CixsWWbaYPO-7WA/view?utm_content=DAG6LdZ1yF0&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h77b333fb75',
     localImage: '/assets/cartoonify-app.avif',
     id: '24'
   },
   {
     title: 'Polaroid Frame Animation',
-    canvaWeb: 'https://www.canva.com/design/DAG6LcCH16Y/GY2kul8Kl7tP6ltWPEoKWw/watch?utm_content=DAG6LcCH16Y&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h035b37dbde',
+    canvaWeb:
+      'https://www.canva.com/design/DAG6LcCH16Y/GY2kul8Kl7tP6ltWPEoKWw/watch?utm_content=DAG6LcCH16Y&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h035b37dbde',
     localImage: '/assets/polaroid-frame-animation.avif',
     id: '25'
   },
   {
     title: 'Sailor',
-    canvaWeb: 'https://www.canva.com/design/DAG6XV7YFKo/-utCA5mKHIGrAzfTUKnxJQ/view?utm_content=DAG6XV7YFKo&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h4b6d476b26',
+    canvaWeb:
+      'https://www.canva.com/design/DAG6XV7YFKo/-utCA5mKHIGrAzfTUKnxJQ/view?utm_content=DAG6XV7YFKo&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h4b6d476b26',
     localImage: '/assets/sailor.avif',
     id: '26'
   },
   {
     title: 'Envelope Effect',
-    canvaWeb: 'https://www.canva.com/design/DAG6jX4vigE/cOGS2MYmxvrCfctZb69Hsw/view?utm_content=DAG6jX4vigE&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h8a7a2abf54',
+    canvaWeb:
+      'https://www.canva.com/design/DAG6jX4vigE/cOGS2MYmxvrCfctZb69Hsw/view?utm_content=DAG6jX4vigE&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h8a7a2abf54',
     localImage: '/assets/envelope-effect.avif',
     id: '27'
   },
   {
     title: 'Element Codes',
-    canvaWeb: 'https://www.canva.com/design/DAG6ub-9aY0/gcL0rNEjvtJLhg0gtWvgPg/view?utm_content=DAG6ub-9aY0&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hdef964079f',
+    canvaWeb:
+      'https://www.canva.com/design/DAG6ub-9aY0/gcL0rNEjvtJLhg0gtWvgPg/view?utm_content=DAG6ub-9aY0&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hdef964079f',
     localImage: '/assets/element-codes.avif',
     id: '28'
   },
   {
     title: '3d on Canva',
-    canvaWeb: 'https://www.canva.com/design/DAG67Nx24e4/ZcUTLCniit7o2Xn6beOkLw/view?utm_content=DAG67Nx24e4&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h874fe8a622',
+    canvaWeb:
+      'https://www.canva.com/design/DAG67Nx24e4/ZcUTLCniit7o2Xn6beOkLw/view?utm_content=DAG67Nx24e4&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h874fe8a622',
     localImage: '/assets/3d-on-canva.avif',
     id: '29'
   },
   {
     title: 'Custom Shadows',
-    canvaWeb: 'https://www.canva.com/design/DAG7ICv_1aU/tb0FWYz9XTjjjo1fiCKxew/view?utm_content=DAG7ICv_1aU&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h002147c5d3',
+    canvaWeb:
+      'https://www.canva.com/design/DAG7ICv_1aU/tb0FWYz9XTjjjo1fiCKxew/view?utm_content=DAG7ICv_1aU&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h002147c5d3',
     localImage: '/assets/custom-shadows.avif',
     id: '30'
   },
   {
     title: 'BrandFetch App',
-    canvaWeb: 'https://www.canva.com/design/DAG7ULiQejQ/u34EAwIejli3exfdLUiPIA/view?utm_content=DAG7ULiQejQ&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hb70bf00721',
+    canvaWeb:
+      'https://www.canva.com/design/DAG7ULiQejQ/u34EAwIejli3exfdLUiPIA/view?utm_content=DAG7ULiQejQ&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hb70bf00721',
     localImage: '/assets/brandfetch-app.avif',
     id: '31'
   },
   {
     title: 'Parts transition',
-    canvaWeb: 'https://www.canva.com/design/DAG7gD_9nYk/UfQ6WyoQxOD01IqCGB0afw/watch?utm_content=DAG7gD_9nYk&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hda047af86c',
+    canvaWeb:
+      'https://www.canva.com/design/DAG7gD_9nYk/UfQ6WyoQxOD01IqCGB0afw/watch?utm_content=DAG7gD_9nYk&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hda047af86c',
     localImage: '/assets/parts-transition.avif',
     id: '32'
   },
   {
     title: 'Random Elements on Canva',
-    canvaWeb: 'https://www.canva.com/design/DAG7qx-Cn_I/WfHVJvFLbTEpsiG0V7T6aQ/view?utm_content=DAG7qx-Cn_I&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h6f237ce674',
+    canvaWeb:
+      'https://www.canva.com/design/DAG7qx-Cn_I/WfHVJvFLbTEpsiG0V7T6aQ/view?utm_content=DAG7qx-Cn_I&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h6f237ce674',
     localImage: '/assets/random-elements-on-canva.avif',
     id: '33'
-  },
+  }
 ];
 
 export const lengthOfProjects = arrayCards.length;
